90076f71d0c8fe02c7abbee3f70c4be1f3fe598a
bricksky <skydh1214@naver.com>
2023-04-13 17:25:18 +0900

    2주차 실습2

--- SeminarWeek2/SeminarWeek2_Assignment/SeminarWeek2_Assignment/SeminarWeek2_Assignment/ViewController.swift
+++ SeminarWeek2/SeminarWeek2_Assignment/SeminarWeek2_Assignment/SeminarWeek2_Assignment/ViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+//    
+//    private lazy var backButton: UIButton = {
+//        let button = UIButton()
+//        button.setImage(UIImage(named: "btn_before.svg"), for: .normal)
+//        button.setImage(UIImage(named: "x-circle 2.svg"), for: .selected)
+//        button.isSelected = true
+//        return button
+//    }()
+//    
+//    private lazy var myImage: UIImageView = {
+//        let imageView = UIImageView()
+//        imageView.image = UIImage(named: "btn_before.svg")
+//        return imageView
+//    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
